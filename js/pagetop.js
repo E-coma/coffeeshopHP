5f38ba93e478fdbd5e0907a22991b43391175846
Emiri <e.coma.125@gmail.com>
2022-01-09 05:14:37 +0900

    スクロールしたらコンセプトのテキストを下から表示

--- js/pagetop.js
+++ js/pagetop.js
@@ -6,10 +6,40 @@ $(".JS_VegasItem").vegas({ // スライドショーにしたい要素指定
     ]
 });
 
+// 右固定のナビゲーション色変更タイミング
+let topimgHeight = document.getElementById("top-img").offsetHeight
 jQuery(window).on('scroll', function () {
-    if (700 < jQuery(this).scrollTop()) {
+    if (topimgHeight <jQuery(this).scrollTop()) {
         jQuery('#header ul a').addClass('change-color');
     } else {
         jQuery('#header ul a').removeClass('change-color');
     }
 });
+
+// 左のロゴ表示タイミング設定
+let $pagetop = $('.ScrollTop');
+
+$(window).on( 'scroll', function () {
+  if ( $(this).scrollTop() < topimgHeight) {
+    $pagetop.removeClass('isActive');
+  } else {
+    $pagetop.addClass('isActive');
+  }
+});
+
+// コンセプトテキスト表示タイミング
+let conceptMarginTop = 170
+let subtitlePadding = 5
+let $conceptUp = document.getElementById("concept-title").offsetHeight + conceptMarginTop + subtitlePadding + document.getElementById("concept-text").offsetHeight;
+
+$(window).on( 'scroll', function () {
+    if ( $(this).scrollTop() < $conceptUp) {
+      $('#concept-text').removeClass('isUp');
+    } else {
+      $('#concept-text').addClass('isUp');
+    }
+  });
+
+  
+// console.log(document.getElementById("concept-title").offsetHeight);
+// console.log(document.getElementById("header-nav").offsetTop);
