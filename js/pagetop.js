106bd4624c5060692cf30bb6579bc4bc8693aeae
Emiri <e.coma.125@gmail.com>
2022-01-09 00:54:02 +0900

    ナビゲーションを一定のスクロールY値で色変更

--- js/pagetop.js
+++ js/pagetop.js
@@ -4,4 +4,12 @@ $(".JS_VegasItem").vegas({ // スライドショーにしたい要素指定
         { src: "common/img/top-img2.jpg" }, // 背景画像2枚目
         { src: "common/img/top-img3.jpg" } // 背景画像3枚目
     ]
-  });
+});
+
+jQuery(window).on('scroll', function () {
+    if (700 < jQuery(this).scrollTop()) {
+        jQuery('#header ul a').addClass('change-color');
+    } else {
+        jQuery('#header ul a').removeClass('change-color');
+    }
+});
