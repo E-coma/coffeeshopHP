948444f876b270003ba67e515915f34ffa0a605e
Emiri <e.coma.125@gmail.com>
2022-01-10 02:46:05 +0900

    テキストのフェードアウトの設定とアクセスとスケジュール追加

--- js/pagetop.js
+++ js/pagetop.js
@@ -29,13 +29,13 @@ $(window).on( 'scroll', function () {
 
 // コンセプトテキスト表示タイミング
 // コンテンツごとのマージントップ↓
-let contentsMarginTop = 170 
-let textMargin = 50
+const contentsMarginTop = 170 
+const textMargin = 50
 // コンセプトタイトルのパディング↓
 let $conceptUp = document.getElementById("concept-title").offsetHeight + contentsMarginTop + textMargin + document.getElementById("concept-text").offsetHeight;
-
+const menuHeigh = document.getElementById("menu").offsetTop
 $(window).on( 'scroll', function () {
-    if ( $(this).scrollTop() < $conceptUp) {
+    if ( $(this).scrollTop() < $conceptUp || $(this).scrollTop() > menuHeigh - contentsMarginTop) {
       $('#concept-text').removeClass('isUp');
     } else {
       $('#concept-text').addClass('isUp');
@@ -43,10 +43,10 @@ $(window).on( 'scroll', function () {
   });
 
 //   メニューテキスト表示タイミング
-let conceptHeight = document.getElementById("concept").offsetHeight
+const conceptHeight = document.getElementById("concept").offsetHeight
 let menu1Up = conceptHeight + contentsMarginTop*2 + textMargin + document.getElementById("coffee-menu").offsetHeight
 $(window).on( 'scroll', function () {
-    if ( $(this).scrollTop() < menu1Up) {
+    if ( $(this).scrollTop() < menu1Up || $(this).scrollTop() > menu1Up + document.getElementById("sandwich-menu").offsetHeight + document.getElementById("sandwich-menu").offsetHeight + textMargin + contentsMarginTop) {
       $('#menu-text1').removeClass('isUp');
     } else {
       $('#menu-text1').addClass('isUp');
@@ -55,12 +55,20 @@ $(window).on( 'scroll', function () {
 
   let menu2Up = menu1Up + textMargin + document.getElementById("sandwich-menu").offsetHeight;
 $(window).on( 'scroll', function () {
-    if ( $(this).scrollTop() < menu2Up) {
+    if ( $(this).scrollTop() < menu2Up || $(this).scrollTop() > menu2Up + contentsMarginTop + textMargin + document.getElementById("access").offsetHeight) {
       $('#menu-text2').removeClass('isUp');
     } else {
       $('#menu-text2').addClass('isUp');
     }
   });
+  let menu3Up = menu2Up + 170*2 + textMargin*2 +  document.getElementById("sandwich-menu").offsetHeight
+  $(window).on( 'scroll', function () {
+    if ( $(this).scrollTop() < menu3Up || $(this).scrollTop() > menu3Up + 80 + textMargin*2 + document.getElementById("access").offsetHeight ) {
+      $('#accesstext').removeClass('isUp');
+    } else {
+      $('#accesstext').addClass('isUp');
+    }
+  });
 
 // console.log(document.getElementById("concept-title").offsetHeight);
-// console.log(document.getElementById("smallTitle1").offsetHeight);
+// console.log(document.getElementById("menu").offsetTop);
